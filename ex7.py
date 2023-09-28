313ae8c4996c8032f6106526d126d363aefb9070
John Dunthorne <johndunthorne1@gmail.com>
2023-09-28 17:04:01 -0500

    1

--- ex7.py
+++ ex7.py
@@ -1,21 +1,20 @@
 print("Mary had a little lamb.")
-2 print("Its fleece was white as {}.".format('snow'))
-3 print("And everywhere that Mary went.")
-4 print("." * 10) # what'd that do?
-5
-6 end1 = "C"
-7 end2 = "h"
-8 end3 = "e"
-9 end4 = "e"
-10 end5 = "s"
-11 end6 = "e"
-12 end7 = "B"
-13 end8 = "u"
-14 end9 = "r"
-15 end10 = "g"
-16 end11 = "e"
-17 end12 = "r"
-18
-19 # watch end = ' ' at the end. try removing it to see what happens
-20 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
-21 print(end7 + end8 + end9 + end10 + end11 + end12)
+print("Its fleece was white as {}.".format('snow'))print("And everywhere that Mary went.")
+print("." * 10) # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
