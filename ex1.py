7a98b5385e641208b19ea63642c4e3a88ce970a0
John Dunthorne <johndunthorne1@gmail.com>
2023-10-27 04:26:10 -0500

    1

--- ex1.py
+++
@@ -1,9 +0,0 @@
-# Page 34
-
-print("Hello World!")
-print("Hello Again")
-print("I like typing this.")
-print("This is fun.")
-print('Yey! Printing.')
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
