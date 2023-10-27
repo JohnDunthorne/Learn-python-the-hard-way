d005aa92ee61e008f0f960292e164ea68fdf7d8e
John Dunthorne <johndunthorne1@gmail.com>
2023-10-27 04:52:50 -0500

    1

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import flask, render_template, request
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    if "name" in request.args:
+        name = request.args("name")
+    else:
+        name = "world"
+    return render_template("index.html")
